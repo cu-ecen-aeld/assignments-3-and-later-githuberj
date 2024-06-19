c2ae0c8dd40bb9dd6c6edcb4e969d8e206334878
Gasthuber Johannes z003e7mx <johannes.gasthuber@siemens.com>
2024-06-19 07:51:59 +0200

    Finished 6-1 with TAILQ

--- server/server.c
+++ server/server.c
@@ -8,33 +8,141 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include "queue.h"
 
 #define PORT 9000
 #define MAX_PACKET_SIZE 1024
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 
-int server_fd;
+#define syslog(priority, ...) \
+    printf(__VA_ARGS__);      \
+    printf("\n");
 
-void handle_signal(int signal) {
-    if (signal == SIGINT || signal == SIGTERM) {
+volatile int server_fd;
+volatile int done = 0;
+
+struct node
+{
+    pthread_t *thread;
+    struct client_data *data;
+    // This macro does the magic to point to other nodes
+    TAILQ_ENTRY(node)
+    nodes;
+};
+
+void handle_signal(int signal)
+{
+    if (signal == SIGINT || signal == SIGTERM)
+    {
         syslog(LOG_INFO, "Caught signal, exiting");
         close(server_fd);
-        remove(DATA_FILE);
-        closelog();
-        exit(0);
+        done = 1;
+        // exit(0);
     }
 }
 
-void send_data_to_client(int client_socket) {
+void send_data_to_client(int client_socket)
+{
     FILE *fp = fopen(DATA_FILE, "r");
     char buffer[1024];
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
         send(client_socket, buffer, strlen(buffer), 0);
     }
     fclose(fp);
 }
 
-int main(int argc, char *argv[]) {
+void read_from_socket_to_file(int client_socket)
+{
+    char buffer[1024];
+    ssize_t bytes_received;
+    FILE *fp = fopen(DATA_FILE, "a");
+
+    while ((bytes_received = recv(client_socket, buffer, sizeof(buffer), 0)) > 0)
+    {
+        buffer[bytes_received] = '\0';
+        fprintf(fp, "%s", buffer);
+
+        char *newline = strchr(buffer, '\n');
+        if (newline != NULL)
+        {
+            fflush(fp);
+            break;
+        }
+    }
+    fclose(fp);
+}
+
+struct client_data
+{
+    int client_socket;
+    char client_ip[INET_ADDRSTRLEN];
+    bool *done;
+};
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *write_timestamp_to_file(void *arg)
+{
+    int i = 0;
+    while (!done)
+    {
+        i++;
+        sleep(1);
+        if (i % 10 == 0)
+        {
+            pthread_mutex_lock(&mutex);
+            FILE *fp = fopen(DATA_FILE, "a");
+            time_t t;
+            struct tm *tmp;
+            char MY_TIME[50];
+            time(&t);
+            char strRFC2822[] = "timestamp:%a, %d %b %Y %T %z\n";
+
+            tmp = localtime(&t);
+            // using strftime to display time
+            strftime(MY_TIME, sizeof(MY_TIME), strRFC2822, tmp);
+            printf("Formatted date & time : %s\n", MY_TIME);
+
+            fprintf(fp, "Timestamp: %s \n", MY_TIME);
+            fclose(fp);
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+
+    return NULL;
+}
+
+void *execute_commands(void *arg)
+{
+    // Code to execute commands goes here
+
+    struct client_data *data = (struct client_data *)arg;
+
+    pthread_mutex_lock(&mutex);
+    read_from_socket_to_file(data->client_socket);
+    pthread_mutex_unlock(&mutex);
+
+    pthread_mutex_lock(&mutex);
+    send_data_to_client(data->client_socket);
+    pthread_mutex_unlock(&mutex);
+
+    close(data->client_socket);
+    syslog(LOG_INFO, "Closed connection from %s", data->client_ip);
+
+    *data->done = 1;
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    TAILQ_HEAD(head_s, node)
+    head;
+    // Initialize the head before use
+    TAILQ_INIT(&head);
     // Set up signal handling
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
@@ -44,37 +152,44 @@ int main(int argc, char *argv[]) {
 
     // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         syslog(LOG_ERR, "Failed to create socket");
         closelog();
         return -1;
     }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+        perror("setsockopt(SO_REUSEADDR) failed");
 
     // Bind socket to port
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1)
+    {
         syslog(LOG_ERR, "Failed to bind socket");
         closelog();
         return -1;
     }
 
-   
     // Daemonize
     bool daemonize = false;
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+    if (argc > 1 && strcmp(argv[1], "-d") == 0)
+    {
         daemonize = true;
     }
-    if (daemonize) {
+    if (daemonize)
+    {
         pid_t pid = fork();
-        if (pid == -1) {
+        if (pid == -1)
+        {
             syslog(LOG_ERR, "Failed to fork");
             closelog();
             return -1;
         }
-        if (pid != 0) {
+        if (pid != 0)
+        {
             // Parent process
             exit(EXIT_SUCCESS);
         }
@@ -82,42 +197,84 @@ int main(int argc, char *argv[]) {
     }
 
     // Listen for connections
-    if (listen(server_fd, 1) == -1) {
+    if (listen(server_fd, 1) == -1)
+    {
         syslog(LOG_ERR, "Failed to listen for connections");
         closelog();
         return -1;
     }
 
-    while (true) {
+    // Start a separate thread to write timestamp to file every 10 seconds
+    pthread_t timestamp_thread;
+    pthread_create(&timestamp_thread, NULL, write_timestamp_to_file, NULL);
+
+    while (true)
+    {
         int new_socket = accept(server_fd, NULL, NULL);
+
+        if (new_socket == -1)
+        {
+            break;
+        }
+
+        bool *done = malloc(sizeof(bool));
+        *done = false;
 
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        char buffer[1024];
-        ssize_t bytes_received;
-        FILE *fp = fopen(DATA_FILE, "a+");
+        pthread_t *thread = (pthread_t *)malloc(sizeof(pthread_t));
+        struct client_data *data = (struct client_data *)malloc(sizeof(struct client_data));
+        data->client_socket = new_socket;
+        strcpy(data->client_ip, client_ip);
+        data->done = done;
+        pthread_create(thread, NULL, execute_commands, (void *)data);
 
-        while ((bytes_received = recv(new_socket, buffer, sizeof(buffer), 0)) > 0) {
-            buffer[bytes_received] = '\0';
-            fprintf(fp, "%s", buffer);
+        // pthread_join(*thread, NULL);
+        struct node *n = malloc(sizeof(struct node));
+        n->thread = thread;
+        n->data = data;
+        TAILQ_INSERT_TAIL(&head, n, nodes);
+        n = NULL;
 
-            char *newline = strchr(buffer, '\n');
-            if (newline != NULL) {
-                fflush(fp);
-                send_data_to_client(new_socket);
-                memset(buffer, 0, sizeof(buffer));
+        n = TAILQ_FIRST(&head);
+
+        while (n != NULL)
+        {
+            if (*(n->data->done) == true)
+            {
+                pthread_join(*(n->thread), NULL);
+                TAILQ_REMOVE(&head, n, nodes);
+                free(n->thread);
+                free(n->data->done);
+                free(n->data);
+                free(n);
             }
+            n = TAILQ_NEXT(n, nodes);
         }
+    }
 
-        close(new_socket);
-        fclose(fp);
-        syslog(LOG_INFO, "Closed connection from %s", client_ip);
+    printf("cleaning up\n");
+
+    pthread_join(timestamp_thread, NULL);
+    struct node *n = NULL;
+
+    while (!TAILQ_EMPTY(&head))
+    {
+        n = TAILQ_FIRST(&head);
+        TAILQ_REMOVE(&head, n, nodes);
+        pthread_join(*(n->thread), NULL);
+        free(n->thread);
+        free(n->data->done);
+        free(n->data);
+        free(n);
+        n = NULL;
     }
 
     // Clean up and exit
     closelog();
     close(server_fd);
+    remove(DATA_FILE);
     return 0;
 }
